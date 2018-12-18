6f5b80d8e204336190fa00aa4e63afdf034a2bbc
Spielley <32324068+Spielley@users.noreply.github.com>
2018-12-18 14:06:45 +0100

    Update basicbot.js
    friendlier params

--- basicbot.js
+++ basicbot.js
@@ -2,9 +2,9 @@ const Coss = require('./index')();
 
 base = {} //database object
 
-tradingsize = 10 // change this to change lotsize
+tradingsize = 2 // change this to change lotsize
 interval =  60 // interval in seconds , 1 loop for each interval
-profitLevel = 2 // profit level in %
+profitLevel = 1 // profit level in %
 tradingpair = 'COSS_ETH' // this is your trading pair
 coinToCheckWallet = 'ETH' // if you run out of this the bot won't buy COSS 
 
